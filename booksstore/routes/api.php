a1b21c31b9df9da9b4efcfe9f5180f08a40eeb17
Анастасия <4kim.an0@gmail.com>
2021-07-16 15:27:30 +1000

    hi2

--- booksstore/routes/api.php
+++ booksstore/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\Api\HelpController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('/products',[HelpController::class, 'index'] );
+//Route::get('/products/{id}',[HelpController::class, 'show'] );
+
+Route::apiResources([
+       'products'=>HelpController::class,
+]);
