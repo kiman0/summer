8bad4f1e52758b855361db2361bc5ff5b59c884f
Анастасия <4kim.an0@gmail.com>
2021-07-17 17:11:43 +1000

    last/last ver.

--- booksstore/app/Http/Controllers/Api/HelpController2.php
+++ booksstore/app/Http/Controllers/Api/HelpController2.php
@@ -31,12 +31,12 @@ class HelpController2 extends Controller
     {
         $created_desk=sessions::create($request->all());
         //$created_desk2=help2::create($request->all());
-        $created_desk2 = help2::create([
+        /**$$created_desk2 = help2::create([
             'bookss_id' => $request['bookss_id'],   // $request->title also works?
             'bookss_count' => $request['bookss_count'], // $request->body also works?
             'sessions_id' =>  sessions::where('session_id')->find('session_id'), // there might be a better solution, but this works 100%
 
-        ]);
+        ]);*/
         return new HelpResource2($created_desk);
     }
 
